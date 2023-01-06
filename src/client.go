bba0855786cb7ea10afb74bb5b313d91b9da9a39
seriouslym <769582861@qq.com>
2023-01-06 22:20:16 +0800

    fix

--- src/client.go
+++ src/client.go
@@ -4,10 +4,12 @@ import (
 	"fmt"
 	"log"
 	"net/rpc"
+	"strconv"
 )
 
 func test() {
-	for i := 0; i < 1000; i++ {
+	for i := 0; i < 50; i++ {
+		fmt.Println("子runtime" + strconv.Itoa(i))
 		client, err := rpc.Dial("tcp", "localhost:1234")
 		if err != nil {
 			log.Fatal("connect err", err)
@@ -17,10 +19,17 @@ func test() {
 		if err != nil {
 			log.Fatal(err)
 		}
-		fmt.Println(reply)
+		log.Println(reply)
 	}
 }
 func main() {
-	fmt.Println(222)
-	go test()
+	done := make(chan struct{})
+	go func() {
+		test()
+		done <- struct{}{}
+	}()
+	for i := 0; i < 50; i++ {
+		fmt.Println(i)
+	}
+	<-done
 }
